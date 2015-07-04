a4c1b6fc3f3e25b4b77a9e4a69e2408099c58714
Vijaya Singh <vijaya.singh@tcs.com>
2015-07-04 10:26:59 +1000

    adding from singh

---
+++ src/main/java/com/qantas/sample/App2.java
@@ -0,0 +1,28 @@
+package com.qantas.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        System.out.println("testing ..");
+
+        
+        System.out.println("vijay_b1 ..");
+        
+        
+        System.out.println("vijay_b1  from to b2 ..");
+        
+        System.out.println("Test");
+
+System.out.println("Test");
+
+        
+
+    }
+}
